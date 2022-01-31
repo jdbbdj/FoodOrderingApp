bef64b502e5c5df44cf69650531c28093690551a
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-01-31 11:34:16 +0800

    firstFix

---
+++ client/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Miekoy's Pizza</title>
+        <meta name="description" content="Best Pizza Shop in Naga City" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      HomePage
+    </div>
+  )
+}
